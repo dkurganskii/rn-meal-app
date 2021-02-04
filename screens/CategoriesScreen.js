8f1160112619ee29c053cafe3bd44b214d020ec9
Dmitrii Kurganskii <44755635+dkurganskii@users.noreply.github.com>
2021-02-03 19:18:24 -0600

    implement navigation to the meal details screen

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -4,7 +4,7 @@ import { View, Text, Button, StyleSheet } from 'react-native'
 const CategoriesScreen = props => {
     return (<View style={styles.screen}>
         <Text>The Categories Screen!</Text>
-        <Button title='Go To Meals!' onPress={() => {
+        <Button title='Go to Meals!' onPress={() => {
             props.navigation.navigate({ routeName: 'CategoryMeals' })
         }} />
     </View>)
--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,9 +1,12 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, Button, StyleSheet } from 'react-native'
 
 const CategoryMealScreen = props => {
     return (<View style={styles.screen}>
         <Text>The Category Meal Screen!</Text>
+        <Button title='Go to details!' onPress={() => {
+            props.navigation.navigate({ routeName: 'MealDetail' })
+        }} />
     </View>)
 }
 
