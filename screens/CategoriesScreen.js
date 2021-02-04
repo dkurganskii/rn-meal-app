901cd9b01e800089a720423a4ec0284d3955c18c
Dmitrii Kurganskii <44755635+dkurganskii@users.noreply.github.com>
2021-02-04 14:53:00 -0600

    configure the header with navigation options

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,19 +1,37 @@
 import React from 'react'
-import { View, Text, FlatList, StyleSheet } from 'react-native'
+import { View, Text, FlatList, StyleSheet, TouchableOpacity, Platform } from 'react-native'
 import { CATEGORIES } from '../data/dummy-data'
+import Colors from '../constants/Colors'
 
-const renderGridItem = (itemData) => {
+const CategoriesScreen = props => {
+    const renderGridItem = (itemData) => {
+        return (
+            <TouchableOpacity
+                style={styles.gridItem}
+                onPress={() => {
+                    props.navigation.navigate({ routeName: 'CategoryMeals' })
+                }}>
+                <View >
+                    <Text>{itemData.item.title}</Text>
+                </View>
+            </TouchableOpacity>
+        )
+    }
     return (
-        <View style={styles.gridItem}>
-            <Text>{itemData.item.title}</Text>
-        </View>
+        <FlatList
+            data={CATEGORIES}
+            renderItem={renderGridItem}
+            numColumns={2}
+        />
     )
 }
 
-const CategoriesScreen = props => {
-    return (
-        <FlatList data={CATEGORIES} renderItem={renderGridItem} numColumns={2} />
-    )
+CategoriesScreen.navigationOptions = {
+    headerTitle: 'Meal Categories',
+    headerStyle: {
+        backgroundColor: Platform.OS === 'android' ? Colors.primaryColor : ''
+    },
+    headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primaryColor
 }
 
 const styles = StyleSheet.create({
