c393f2dba946a129fa0de1c6251b642ddbac5862
Dmitrii Kurganskii <44755635+dkurganskii@users.noreply.github.com>
2021-02-06 14:30:17 -0600

    load meals for categories

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,25 +1,31 @@
 import React from 'react'
-import { View, Text, Button, StyleSheet } from 'react-native'
-
-import { CATEGORIES } from '../data/dummy-data'
+import { View, Text, FlatList, StyleSheet } from 'react-native'
+import { CATEGORIES, MEALS } from '../data/dummy-data'
 
 const CategoryMealScreen = props => {
+    const renderMealItem = itemData => {
+        return (
+            <View><Text>{itemData.item.title}</Text></View>
+        )
+    }
+
     const catId = props.navigation.getParam('categoryId')
 
     const selectedCategory = CATEGORIES.find(cat => cat.id === catId)
-    return (<View style={styles.screen}>
-        <Text>The Category Meal Screen!</Text>
-        <Text>{selectedCategory.title}</Text>
-        <Button title='Go to details!' onPress={() => {
-            props.navigation.navigate({ routeName: 'MealDetail' })
-        }} />
-        <Button title='Go back' onPress={() => { props.navigation.goBack() }} />
-    </View>)
+    const displayedMeals = MEALS.filter(meal => meal.categoryIds.indexOf(catId) >= 0)
+    return (
+        <View style={styles.screen}>
+            <FlatList
+                data={displayedMeals}
+                renderItem={renderMealItem} />
+        </View>
+    )
 }
 
 CategoryMealScreen.navigationOptions = (navigationData) => {
     const catId = navigationData.navigation.getParam('categoryId')
     const selectedCategory = CATEGORIES.find(cat => cat.id === catId)
+
     return {
         headerTitle: selectedCategory.title,
         headerStyle: {
