b4541944fef37e829a0a158e3754cc0dbd076077
Dmitrii Kurganskii <44755635+dkurganskii@users.noreply.github.com>
2021-02-09 10:03:33 -0600

    add Favorites stack

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,17 +1,24 @@
 import React from 'react';
-import { createStackNavigator } from 'react-navigation-stack';
-import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { Platform } from 'react-native';
 import { createAppContainer } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs'
+import { createStackNavigator } from 'react-navigation-stack'
+import { Ionicons } from '@expo/vector-icons';
+import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs';
 
 import CategoriesScreen from '../screens/CategoriesScreen';
-import CategoryMealsScreen from '../screens/CategoryMealsScreen'
-import MealDetailScreen from '../screens/MealDetailScreen'
-import FavoritesScreen from '../screens/FavoritesScreen'
-import { Platform } from 'react-native'
-import { Ionicons } from '@expo/vector-icons';
-import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs'
-import Colors from '../constants/Colors'
+import CategoryMealsScreen from '../screens/CategoryMealsScreen';
+import MealDetailScreen from '../screens/MealDetailScreen';
+import FavoritesScreen from '../screens/FavoritesScreen';
+import Colors from '../constants/Colors';
 
+const defaultStackNavOptions = {
+    headerStyle: {
+        backgroundColor: Platform.OS === 'android' ? Colors.primaryColor : ''
+    },
+    headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primaryColor,
+    headerTitle: 'A Screen'
+};
 
 const MealsNavigator = createStackNavigator(
     {
@@ -25,14 +32,18 @@ const MealsNavigator = createStackNavigator(
     },
     {
         // initialRouteName: 'Categories',
-        defaultNavigationOptions: {
-            headerStyle: {
-                backgroundColor: Platform.OS === 'android' ? Colors.primaryColor : ''
-            },
-            headerTintColor:
-                Platform.OS === 'android' ? 'white' : Colors.primaryColor,
-            headerTitle: 'A Screen'
-        }
+        defaultNavigationOptions: defaultStackNavOptions
+    }
+);
+
+const FavNavigator = createStackNavigator(
+    {
+        Favorites: FavoritesScreen,
+        MealDetail: MealDetailScreen
+    },
+    {
+        // initialRouteName: 'Categories',
+        defaultNavigationOptions: defaultStackNavOptions
     }
 );
 
@@ -49,7 +60,7 @@ const tabScreenConfig = {
         }
     },
     Favorites: {
-        screen: FavoritesScreen,
+        screen: FavNavigator,
         navigationOptions: {
             tabBarIcon: tabInfo => {
                 return <Ionicons name="ios-star" size={25} color={tabInfo.tintColor} />;
