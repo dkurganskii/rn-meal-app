cf00abf3ee185a14585ba719f7af8c3bb3198dec
Dmitrii Kurganskii <44755635+dkurganskii@users.noreply.github.com>
2021-02-09 10:32:36 -0600

    add a menu button and drawer navigation

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -3,10 +3,12 @@ import { Platform } from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { createBottomTabNavigator } from 'react-navigation-tabs'
 import { createStackNavigator } from 'react-navigation-stack'
+import { createDrawerNavigator } from 'react-navigation-drawer'
 import { Ionicons } from '@expo/vector-icons';
 import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs';
 
 import CategoriesScreen from '../screens/CategoriesScreen';
+import FiltersScreen from '../screens/FiltersScreen'
 import CategoryMealsScreen from '../screens/CategoryMealsScreen';
 import MealDetailScreen from '../screens/MealDetailScreen';
 import FavoritesScreen from '../screens/FavoritesScreen';
@@ -85,4 +87,36 @@ const MealsFavTabNavigator =
             }
         });
 
-export default createAppContainer(MealsFavTabNavigator);
+const FiltersNavigator = createStackNavigator(
+    {
+        Filters: FiltersScreen
+    },
+    {
+        // navigationOptions: {
+        //   drawerLabel: 'Filters!!!!'
+        // },
+        defaultNavigationOptions: defaultStackNavOptions
+    }
+);
+
+const MainNavigator = createDrawerNavigator(
+    {
+        MealsFavs: {
+            screen: MealsFavTabNavigator,
+            navigationOptions: {
+                drawerLabel: 'Meals'
+            }
+        },
+        Filters: FiltersNavigator
+    },
+    {
+        contentOptions: {
+            activeTintColor: Colors.accentColor,
+            labelStyle: {
+                fontFamily: 'open-sans-bold'
+            }
+        }
+    }
+);
+
+export default createAppContainer(MainNavigator);
