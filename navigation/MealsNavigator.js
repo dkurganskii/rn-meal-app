90a0ab7c95d45e20c780db6807042f13ce11681c
Dmitrii Kurganskii <44755635+dkurganskii@users.noreply.github.com>
2021-02-09 12:25:42 -0600

    configure the drawer

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -18,6 +18,12 @@ const defaultStackNavOptions = {
     headerStyle: {
         backgroundColor: Platform.OS === 'android' ? Colors.primaryColor : ''
     },
+    headerTitleStyle: {
+        fontFamily: 'open-sans-bold'
+    },
+    headerBackTitleStyle: {
+        fontFamily: 'open-sans'
+    },
     headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primaryColor,
     headerTitle: 'A Screen'
 };
@@ -58,7 +64,13 @@ const tabScreenConfig = {
                     <Ionicons name="ios-restaurant" size={25} color={tabInfo.tintColor} />
                 );
             },
-            tabBarColor: Colors.primaryColor
+            tabBarColor: Colors.primaryColor,
+            tabBarLabel:
+                Platform.OS === 'android' ? (
+                    <Text style={{ fontFamily: 'open-sans-bold' }}>Meals</Text>
+                ) : (
+                        'Meals'
+                    )
         }
     },
     Favorites: {
@@ -67,7 +79,13 @@ const tabScreenConfig = {
             tabBarIcon: tabInfo => {
                 return <Ionicons name="ios-star" size={25} color={tabInfo.tintColor} />;
             },
-            tabBarColor: Colors.accentColor
+            tabBarColor: Colors.accentColor,
+            tabBarLabel:
+                Platform.OS === 'android' ? (
+                    <Text style={{ fontFamily: 'open-sans-bold' }}>Favorites</Text>
+                ) : (
+                        'Favorites'
+                    )
         }
     }
 };
@@ -83,6 +101,9 @@ const MealsFavTabNavigator =
         })
         : createBottomTabNavigator(tabScreenConfig, {
             tabBarOptions: {
+                labelStyle: {
+                    fontFamily: 'open-sans'
+                },
                 activeTintColor: Colors.accentColor
             }
         });
