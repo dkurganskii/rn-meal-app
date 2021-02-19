8ffb2fe3fb3ab5cdd2272ec5f49e8ff6b43fa1cd
Dmitrii Kurganskii <44755635+dkurganskii@users.noreply.github.com>
2021-02-19 15:10:03 -0600

    add redux data and navigation options

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { ScrollView, View, Image, Text, StyleSheet } from 'react-native';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 
@@ -17,6 +17,10 @@ const MealDetailScreen = props => {
     const mealId = props.navigation.getParam('mealId');
 
     const selectedMeal = availableMeals.find(meal => meal.id === mealId);
+
+    // useEffect(() => {
+    //     props.navigation.setParams({ mealTitle: selectedMeal.title })
+    // }, [selectedMeal])
 
     return (
         <ScrollView>
@@ -39,9 +43,10 @@ const MealDetailScreen = props => {
 
 MealDetailScreen.navigationOptions = navigationData => {
     const mealId = navigationData.navigation.getParam('mealId');
-    const selectedMeal = MEALS.find(meal => meal.id === mealId);
+    const mealTitle = navigationData.navigation.getParam('mealTitle')
+    // const selectedMeal = MEALS.find(meal => meal.id === mealId);
     return {
-        headerTitle: selectedMeal.title,
+        headerTitle: mealTitle,
         headerRight: (
             <HeaderButtons HeaderButtonComponent={HeaderButton}>
                 <Item
